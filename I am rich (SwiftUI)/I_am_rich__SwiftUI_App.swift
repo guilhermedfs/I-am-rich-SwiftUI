54a5603a8c6e3058adc77389768403f5d646ccec
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2021-12-15 08:37:53 -0300

    Initial Commit

---
+++ I am rich (SwiftUI)/I_am_rich__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_rich__SwiftUI_App.swift
+//  I am rich (SwiftUI)
+//
+//  Created by Guilherme Fernandes - pessoal on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct I_am_rich__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
